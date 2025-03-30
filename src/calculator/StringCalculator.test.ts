eb5dc75b982eec3adcec031aa3e729c71e3e7584
Vinay <vin070@gmail.com>
2025-03-30 12:18:04 +0530

    add failing test

--- src/calculator/StringCalculator.test.ts
+++ src/calculator/StringCalculator.test.ts
@@ -6,5 +6,10 @@ describe('StringCalculator', () => {
         it('should return 0 for empty string', () => {
             expect(add('')).toBe(0);
         });
+        
+        it('should return the number for single number input', () => {
+            expect(add('1')).toBe(1);
+            expect(add('2')).toBe(2);
+        });
     });    
 }); 
